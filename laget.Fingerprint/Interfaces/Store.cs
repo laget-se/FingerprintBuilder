f2fa4eba7178a495039b26a23447425214310f01
Erik Hennerfors <erik.hennerfors@obscured.se>
2021-02-15 05:32:04 +0100

    updates

---
+++ laget.Fingerprint/Interfaces/Store.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace laget.Fingerprint.Interfaces
+{
+    public interface IStore<TFingerprint> where TFingerprint : IFingerprint
+    {
+        void Add(TFingerprint model);
+        TFingerprint Get(string hash);
+        void Remove(string hash);
+        bool Exists(string hash);
+
+        IEnumerable<TFingerprint> Items { get; }
+    }
+}
